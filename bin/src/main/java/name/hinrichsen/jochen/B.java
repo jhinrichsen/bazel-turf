0874efb0ec610f657cd065821dda1bfa8a75364f
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2017-01-07 14:40:19 +0100

    feat: add method call into lib

--- bin/src/main/java/name/hinrichsen/jochen/B.java
+++ bin/src/main/java/name/hinrichsen/jochen/B.java
@@ -6,6 +6,7 @@ import name.hinrichsen.jochen.A;
 public class B {
   public static void main(String args[]) {
     System.out.println("Hello B, it is now " + new A().getNow());
+    System.out.println("Twice 2 equals " + A.twice(2));
   }
 }
 
